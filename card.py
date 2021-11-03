b0febb7b21892aa4ef6367787a2e160a692f6f2c
y65ng <74285192+Y65NG@users.noreply.github.com>
2021-11-03 09:50:57 +0800

    Initialize 三国杀

---
+++ card.py
@@ -0,0 +1,11 @@
+import random as rd
+
+SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+
+class Card:
+    def __init__(self, suit, point):
+        self.suit = suit
+        self.point = point
+
+
+        
